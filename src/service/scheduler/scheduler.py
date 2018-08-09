6c2e8fa311c11212b50c799df152816c22fa268e
Gilvan <gilvanritter@gmail.com>
2018-08-09 08:12:04 -0300

    implemented _process_tracks methods which organize tracks in shifts in their order

--- src/service/scheduler/scheduler.py
+++ src/service/scheduler/scheduler.py
@@ -1,4 +1,5 @@
 from src.entity.shift import Shift
+from src.entity.track import Track
 from src.service.scheduler.bin_packing import BinPacking
 
 __author__ = 'gritt'
@@ -22,16 +23,14 @@ class Scheduler:
             self.AFTERNOON_SHIFT
         ]
 
-        # self.total_time = 0
-        # self.shifts_number = 0
-
-        # self.tracks = []
+        self.tracks = []
         self.shifts = []
         self.talks = []
 
     def schedule(self, talks):
         self.talks = talks
         self._process_shifts()
+        self._process_tracks()
 
     def _process_shifts(self):
 
@@ -79,8 +78,70 @@ class Scheduler:
                     current_shift_option = 0
                     continue
 
-        # unclosed shift (unprocessed remaining time) also must be added
+        # unclosed shift (has remaining time) also must be added
         if current_shift != 0:
             self.shifts.append(current_shift)
 
         return self.shifts
+
+    def _process_tracks(self):
+
+        # starts with a morning shift
+        current_shift_option = 0
+        current_track_day = 1
+
+        current_track = 0
+
+        while len(self.shifts) > 0:
+
+            try:
+
+                index = 0
+                for shift in self.shifts:
+
+                    if current_track == 0:
+                        current_track = Track()
+                        current_track.set_day(current_track_day)
+                        current_track.set_shifts_limit(len(self.shift_options))
+
+                    # matched a needed shift (same time), must start with a morning shift
+                    if shift.get_minutes() == self.shift_options[current_shift_option]:
+
+                        # adds the current shift in the track
+                        current_track.add_shift(shift)
+                        # remove shift from available ones
+                        self.shifts.pop(index)
+
+                        # close this track, completed
+                        if current_track.shifts_limit == len(current_track.shifts):
+                            # saves the current track which has been completed
+                            self.tracks.append(current_track)
+                            current_track = 0
+                            current_track_day += 1
+
+                        # next shift must be opposite
+                        if current_shift_option == 0:
+                            current_shift_option = 1
+                            index += 1
+                            continue
+                        if current_shift_option == 1:
+                            current_shift_option = 0
+                            index += 1
+                            continue
+
+                    index += 1
+
+            except Exception as ex:
+                # saves the current track which has been completed
+                self.tracks.append(current_track)
+
+                # resets so a new track will be created
+                current_track = 0
+                current_track_day += 1
+
+        # unclosed track (has remaining shift(s)) also must be added
+        if current_track != 0:
+            if len(current_track.shifts) > 0:
+                self.tracks.append(current_track)
+
+        return self.tracks
