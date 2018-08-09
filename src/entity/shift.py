3ff8251ab1e502af021defdd23cd274f7a8126be
Gilvan <gilvanritter@gmail.com>
2018-08-09 01:33:53 -0300

    added properties to shifts, start_time, end_time, minutes, used_minutes and multiple talks

--- src/entity/shift.py
+++ src/entity/shift.py
@@ -4,4 +4,10 @@ __author__ = 'gritt'
 class Shift:
 
     def __init__(self):
-        pass
+        self.start_time = 0
+        self.end_time = 0
+
+        self.minutes = 0
+        self.used_minutes = 0
+
+        self.talks = []
