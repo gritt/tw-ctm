8f2b08faed8d7ce524cad9b71db3f198566575f4
Gilvan <gilvanritter@gmail.com>
2018-08-09 04:47:27 -0300

    created methods to set minutes, get remaining_minutes and add a talk to a shift

--- src/entity/shift.py
+++ src/entity/shift.py
@@ -8,6 +8,20 @@ class Shift:
         self.end_time = 0
 
         self.minutes = 0
-        self.used_minutes = 0
+        self.remaining_minutes = 0
 
         self.talks = []
+
+    def set_minutes(self, minutes):
+        self.minutes = minutes
+        self.remaining_minutes = minutes
+
+    def get_remaining_minutes(self):
+        return self.remaining_minutes
+
+    def add_talk(self, talk):
+        if self.remaining_minutes < talk.get_duration():
+            raise Exception("Error: this talk does not fit in this shift")
+
+        self.remaining_minutes -= talk.get_duration()
+        self.talks.append(talk)
