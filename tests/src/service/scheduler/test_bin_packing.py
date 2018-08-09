6e3c4bd0c65f863f8bbd75b2c716af599a39ce4d
Gilvan <gilvanritter@gmail.com>
2018-08-09 03:57:57 -0300

    created test case for BinPacking class

--- tests/src/service/scheduler/test_bin_packing.py
+++ tests/src/service/scheduler/test_bin_packing.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.entity.talk import Talk
 from src.service.scheduler.bin_packing import BinPacking
 
 
@@ -23,6 +24,81 @@ class BinPackingTestCase(unittest.TestCase):
     @unittest.expectedFailure
     def test_calculate_fittingness_failure(self):
         self.bin_packing._calculate_fittingness(130, 150)
+
+    def test_get_best_fitting_talk(self):
+        talks = []
+
+        talk1 = Talk()
+        talk1.set_duration(45)
+
+        talk2 = Talk()
+        talk2.set_duration(38)
+
+        talk3 = Talk()
+        talk3.set_duration(64)
+
+        talk4 = Talk()
+        talk4.set_duration(54)
+
+        talks.append(talk1)
+        talks.append(talk2)
+        talks.append(talk3)
+        talks.append(talk4)
+
+        remaining_bin_size = 60
+
+        self.assertEquals(
+            3,
+            self.bin_packing.get_best_fitting_talk(remaining_bin_size, talks)
+        )
+
+    def test_get_best_fitting_talk_loop(self):
+        talks = []
+
+        talk1 = Talk()
+        talk1.set_duration(35)
+
+        talk2 = Talk()
+        talk2.set_duration(30)
+
+        talk3 = Talk()
+        talk3.set_duration(40)
+
+        talks.append(talk1)
+        talks.append(talk2)
+        talks.append(talk3)
+
+        remaining_bin_size = 85
+
+        self.assertEquals(
+            2,
+            self.bin_packing.get_best_fitting_talk(remaining_bin_size, talks)
+        )
+
+        talks.pop(2)
+        remaining_bin_size = 85 - 40
+
+        self.assertEquals(
+            0,
+            self.bin_packing.get_best_fitting_talk(remaining_bin_size, talks)
+        )
+
+    @unittest.expectedFailure
+    def test_get_best_fitting_talk_failure(self):
+        talks = []
+
+        talk1 = Talk()
+        talk1.set_duration(80)
+
+        talk2 = Talk()
+        talk2.set_duration(98)
+
+        talks.append(talk1)
+        talks.append(talk2)
+
+        remaining_bin_size = 79
+
+        self.bin_packing.get_best_fitting_talk(remaining_bin_size, talks)
 
 
 if __name__ == '__main__':
