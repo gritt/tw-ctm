93669a082421db53ee44c108f260ad09321396f8
Gilvan <gilvanritter@gmail.com>
2018-08-09 08:11:10 -0300

    added unit tests to process_tracks method

--- tests/src/service/scheduler/test_scheduler.py
+++ tests/src/service/scheduler/test_scheduler.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.entity.shift import Shift
 from src.entity.talk import Talk
 from src.service.scheduler.scheduler import Scheduler
 
@@ -78,6 +79,164 @@ class SchedulerTestCase(unittest.TestCase):
         self.assertEquals(
             shifts[1].talks[0].get_name(), "C"
         )
+
+    def test_process_tracks(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shift3 = Shift()
+        shift3.set_minutes(240)
+
+        shift4 = Shift()
+        shift4.set_minutes(180)
+
+        shift5 = Shift()
+        shift5.set_minutes(180)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+        shifts.append(shift3)
+        shifts.append(shift4)
+        shifts.append(shift5)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 3 days
+        self.assertEquals(3, len(tracks))
+
+    def test_process_tracks_one_day(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 1 days
+        self.assertEquals(1, len(tracks))
+
+    def test_process_tracks_one_day_and_half(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shift3 = Shift()
+        shift3.set_minutes(180)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+        shifts.append(shift3)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 1 days
+        self.assertEquals(2, len(tracks))
+
+    def test_process_tracks_two_days(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shift3 = Shift()
+        shift3.set_minutes(240)
+
+        shift4 = Shift()
+        shift4.set_minutes(180)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+        shifts.append(shift3)
+        shifts.append(shift4)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 2 days
+        self.assertEquals(2, len(tracks))
+
+    def test_process_tracks_two_days_and_half(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shift3 = Shift()
+        shift3.set_minutes(240)
+
+        shift4 = Shift()
+        shift4.set_minutes(180)
+
+        shift5 = Shift()
+        shift5.set_minutes(180)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+        shifts.append(shift3)
+        shifts.append(shift4)
+        shifts.append(shift5)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 3 days
+        self.assertEquals(3, len(tracks))
+
+    def test_process_tracks_four_days_and_half(self):
+        shift1 = Shift()
+        shift1.set_minutes(180)
+
+        shift2 = Shift()
+        shift2.set_minutes(240)
+
+        shift3 = Shift()
+        shift3.set_minutes(240)
+
+        shift4 = Shift()
+        shift4.set_minutes(180)
+
+        shift6 = Shift()
+        shift6.set_minutes(180)
+
+        shift5 = Shift()
+        shift5.set_minutes(240)
+
+        shift7 = Shift()
+        shift7.set_minutes(180)
+
+        shifts = []
+        shifts.append(shift1)
+        shifts.append(shift2)
+        shifts.append(shift3)
+        shifts.append(shift4)
+        shifts.append(shift5)
+        shifts.append(shift6)
+        shifts.append(shift7)
+
+        self.scheduler.shifts = shifts
+        tracks = self.scheduler._process_tracks()
+
+        # 4 days
+        self.assertEquals(4, len(tracks))
 
 
 if __name__ == '__main__':
