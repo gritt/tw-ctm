8a6248767f084a237e43f3fa4e7023fe24928b7b
Gilvan <gilvanritter@gmail.com>
2018-08-09 08:12:56 -0300

    implemented set_day, set_shifts_limit and add_shift

--- src/entity/track.py
+++ src/entity/track.py
@@ -6,3 +6,19 @@ class Track:
     def __init__(self):
         self.day = 0
         self.shifts = []
+        self.shifts_limit = 0
+
+    def set_day(self, day):
+        self.day = day
+
+    def set_shifts_limit(self, limit):
+        self.shifts_limit = limit
+
+    def add_shift(self, shift):
+
+        if len(self.shifts) == self.shifts_limit:
+            raise Exception("Error: reached the max amount of shifts for this track")
+
+        self.shifts.append(shift)
+
+
