b789b1d9ad83f231167015324cd1ff2da10bcd28
Gilvan <gilvanritter@gmail.com>
2018-08-09 08:17:33 -0300

    added method to get_day and get_shifts

--- src/entity/track.py
+++ src/entity/track.py
@@ -11,14 +11,17 @@ class Track:
     def set_day(self, day):
         self.day = day
 
+    def get_day(self):
+        return self.day
+
     def set_shifts_limit(self, limit):
         self.shifts_limit = limit
 
     def add_shift(self, shift):
-
         if len(self.shifts) == self.shifts_limit:
             raise Exception("Error: reached the max amount of shifts for this track")
 
         self.shifts.append(shift)
 
-
+    def get_shifts(self):
+        return self.shifts
