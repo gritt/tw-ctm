c544d9963dc387d01a22da85d55bc16e92b3c589
Gilvan <gilvanritter@gmail.com>
2018-08-09 03:10:05 -0300

    implemented method to calculate_fittingness_percentage for a given bin size and pack size

--- src/service/scheduler/bin_packing.py
+++ src/service/scheduler/bin_packing.py
@@ -5,3 +5,9 @@ class BinPacking:
 
     def __init__(self):
         pass
+
+    def calculate_fittingness_percentage(self, bin_size, pack_size):
+        if bin_size < pack_size:
+            raise Exception("Error: this package is too large for this bin")
+
+        return (pack_size * 100) / bin_size
